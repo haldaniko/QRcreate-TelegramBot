c6add21a998ddc5d1299d0811d357209b2ac9c1b
haldaniko <haldaniko@gmail.com>
2024-09-27 00:03:52 +0200

    delete unnecessary comments

--- QRcode.py
+++ QRcode.py
@@ -1,7 +1,6 @@
 import pyqrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
-import png
 
 
 def qrCreate(picture_name, link):
@@ -12,8 +11,3 @@ def qrCreate(picture_name, link):
 def qrDecode(picture_path):
     decocdeQR = decode(Image.open(picture_path))
     return decocdeQR[0].data.decode('ascii')
-
-# pip install PyQRCode
-# pip install pypng
-# pip install pyzbar
-# pip install pillow
